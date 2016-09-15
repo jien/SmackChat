1d7bb283accfb0bda4ec548ef254d128de804fc1
liwenlong <liwenlong@angejia.com>
2016-09-16 05:06:54 +0800

    添加smack library，添加module

---
+++ library/src/main/java/com/jien/smack/chat/library/core/ChatManager.java
@@ -0,0 +1,9 @@
+package com.jien.smack.chat.library.core;
+
+/**
+ * Created by liwenlong on 16/9/6.
+ */
+
+public class ChatManager {
+
+}
---
+++ library/src/main/java/com/jien/smack/chat/library/util/ChatUtil.java
@@ -0,0 +1,8 @@
+package com.jien.smack.chat.library.util;
+
+/**
+ * Created by liwenlong on 16/9/6.
+ */
+
+public class ChatUtil {
+}
